d835f28a7ed85db49691573addf0dcbc9f00c5e2
Victor Dracxler <victordracxler@poli.ufrj.br>
2022-12-14 19:28:48 -0300

    Exercises 1-3

---
+++ Prática - Exercícios Join/01.sql
@@ -0,0 +1,4 @@
+SELECT users.id, users.name, cities.name AS "city"
+FROM users JOIN cities
+ON users."cityId" = cities.id
+WHERE cities.id = 3658;
---
+++ Prática - Exercícios Join/02.sql
@@ -0,0 +1,5 @@
+SELECT testimonials.id, u1.name AS "writer", u2.name AS "recipient", testimonials.message 
+FROM testimonials JOIN users u1
+ON testimonials."writerId" = u1.id
+JOIN users u2
+ON testimonials."recipientId" = u2.id;
